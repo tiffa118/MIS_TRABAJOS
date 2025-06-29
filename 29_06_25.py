389d5fbc00fb70f769fd068e051645d70642c21a
sheyla villacorta <esthefanyavillamolina@gmail.com>
2025-06-29 12:23:15 -0500

    mi primera practica

---
+++ 29_06_25.py
@@ -0,0 +1,11 @@
+# Mi Primera Práctica
+
+Este proyecto representa mi primera práctica realizada como parte de mi proceso de aprendizaje y desarrollo profesional.
+
+## Autora
+
+estudiante
+
+## Año
+
+2025
